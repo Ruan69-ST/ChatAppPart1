3f0858ff558ecde8a2afa9528894108864103e0b
Ruan69-ST <ST10535597@myemeris.edu.za>
2026-04-20 21:58:21 +0200

    Created Login class with attributes

---
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+/**
+ *
+ * @author Ruan
+ */
+public class Login {
+    
+    private String username;
+    private String password;
+    private String cellPhone;
+    private String firstName;
+    private String lastName;
+    
+public Login(String firstName, String lastName, String username, String password, String cellPhone) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.cellPhone = cellPhone;
+}
+}
