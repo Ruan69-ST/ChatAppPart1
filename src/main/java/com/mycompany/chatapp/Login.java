253b5882349c8d93a780614cc6e0514b36947fb1
Ruan69-ST <ST10535597@myemeris.edu.za>
2026-04-20 22:24:02 +0200

    Added validation for cell number

--- src/main/java/com/mycompany/chatapp/Login.java
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -26,4 +26,9 @@ public Login(String firstName, String lastName, String username, String password
 public boolean checkUserName() {
     return username.contains("_") && username.length() <=5;
 }
+//Validates password with regex: must be more than 8 characters, include a capital letter, number, and a special character.
+public boolean checkPasswordComplexity() {
+    String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+    return java.util.regex.Pattern.matches(regex, password);
+}
 }
