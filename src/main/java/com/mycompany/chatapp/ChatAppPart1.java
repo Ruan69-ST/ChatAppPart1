993db497d0dccd97564cf34a18a74db6d97ec059
Ruan69-ST <ST10535597@myemeris.edu.za>
2026-04-21 20:24:44 +0200

    Completed console application for registration and login

--- src/main/java/com/mycompany/chatapp/ChatAppPart1.java
+++ src/main/java/com/mycompany/chatapp/ChatAppPart1.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.chatapp;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,36 @@ package com.mycompany.chatapp;
 public class ChatAppPart1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter cell phone number: ");
+        String cellPhone = input.nextLine();
+
+        Login user = new Login(firstName, lastName, username, password, cellPhone);
+
+        System.out.println(user.registerUser());
+
+        System.out.print("Enter username to login: ");
+        String enteredUsername = input.nextLine();
+
+        System.out.print("Enter password to login: ");
+        String enteredPassword = input.nextLine();
+
+        boolean loginSuccess = user.loginUser(enteredUsername, enteredPassword);
+
+        System.out.println(user.returnLoginStatus(loginSuccess));
     }
 }
